0dc1c3788481e4e863f90f3291234649177fed32
Pranjal <rawat.pranjal007@gmail.com>
2023-02-16 01:21:33 -0500

    sdfds

---
+++ dynare/lucas78/+model/static_resid.m
@@ -0,0 +1,31 @@
+function residual = static_resid(T, y, x, params, T_flag)
+% function residual = static_resid(T, y, x, params, T_flag)
+%
+% File created by Dynare Preprocessor from .mod file
+%
+% Inputs:
+%   T         [#temp variables by 1]  double   vector of temporary terms to be filled by function
+%   y         [M_.endo_nbr by 1]      double   vector of endogenous variables in declaration order
+%   x         [M_.exo_nbr by 1]       double   vector of exogenous variables in declaration order
+%   params    [M_.param_nbr by 1]     double   vector of parameter values in declaration order
+%                                              to evaluate the model
+%   T_flag    boolean                 boolean  flag saying whether or not to calculate temporary terms
+%
+% Output:
+%   residual
+%
+
+if T_flag
+    T = model.static_resid_tt(T, y, x, params);
+end
+residual = zeros(2, 1);
+lhs = y(2);
+rhs = exp(params(4)*log(y(2))+params(3)*x(1));
+residual(1) = lhs - rhs;
+lhs = y(1)*T(1);
+rhs = T(1)*params(1)*(y(2)+y(1));
+residual(2) = lhs - rhs;
+if ~isreal(residual)
+  residual = real(residual)+imag(residual).^2;
+end
+end
